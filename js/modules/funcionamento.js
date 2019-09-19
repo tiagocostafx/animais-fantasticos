8e2a9488cefca3b8421b0dfe2983ffa2c01c1f08
Tiago Costa <tiagocorreiacosta@gmail.com>
2019-09-18 22:08:47 -0300

    Primeiro Commit

---
+++ js/modules/funcionamento.js
@@ -0,0 +1,32 @@
+export default function initFuncionamento() {
+  const funcionamento = document.querySelector('[data-semana]');
+  const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
+  const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+
+  const dataAgora = new Date();
+  const diaAgora = dataAgora.getDay();
+  const horarioAgora = dataAgora.getHours();
+
+  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+  const horarioAberto = horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
+
+  if (semanaAberto && horarioAberto) {
+    funcionamento.classList.add('aberto');
+  }
+}
+
+// const agora = new Date();
+// const futuro = new Date('Dec 31 2019');
+
+// console.log(agora.getDate())
+
+// console.log(futuro)
+
+// function transformarDias(tempo){
+//   return tempo / (60 * 60 * 1000);
+// }
+
+// const diasAgora = transformarDias(agora.getTime());
+// const diasFuturo = transformarDias(futuro.getTime());
+
+// console.log(diasFuturo - diasAgora);
