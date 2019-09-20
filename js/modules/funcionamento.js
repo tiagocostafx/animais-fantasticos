eef709f8199f552e46f6b6e781c8573c7888ff50
Tiago Costa <tiagocorreiacosta@gmail.com>
2019-09-20 17:59:48 -0300

    correcao em dadosAgora

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 export default class Funcionamento {
   constructor(funcionamento, activeClass) {
     this.funcionamento = document.querySelector(funcionamento);
@@ -7,18 +6,20 @@ export default class Funcionamento {
 
   dadosFuncionamento() {
     this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number);
+
     this.horarioSemana = this.funcionamento.dataset.horario.split(',').map(Number);
   }
 
   dadosAgora() {
     this.dataAgora = new Date();
-    this.iaAgora = this.dataAgora.getDay();
+    this.diaAgora = this.dataAgora.getDay();
     this.horarioAgora = this.dataAgora.getUTCHours() - 3;
   }
 
   estaAberto() {
     const semanaAberto = this.diasSemana.indexOf(this.diaAgora) !== -1;
-    const horarioAberto = this.horarioAgora >= this.horarioSemana[0] && this.horarioAgora < this.horarioSemana[1];
+    const horarioAberto = (this.horarioAgora >= this.horarioSemana[0]
+      && this.horarioAgora < this.horarioSemana[1]);
     return semanaAberto && horarioAberto;
   }
 
